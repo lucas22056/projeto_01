ca9db1d8b2e34003904743f30fad92456a649515
Lucas Carvalho <lucashot22056@gmail.com>
2020-02-27 15:11:07 -0300

    Updates

---
+++ precache-manifest.cd88a73fc118136c08e3e493f753dd22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a1fb18848ad9766002c81fbea829b56",
+    "url": "/projeto_01/index.html"
+  },
+  {
+    "revision": "47ad5af8b1e589b569e4",
+    "url": "/projeto_01/static/css/2.5ffa1744.chunk.css"
+  },
+  {
+    "revision": "0f673b12560624049f5e",
+    "url": "/projeto_01/static/css/main.d0c0bd8d.chunk.css"
+  },
+  {
+    "revision": "47ad5af8b1e589b569e4",
+    "url": "/projeto_01/static/js/2.3b92dee2.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/projeto_01/static/js/2.3b92dee2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f673b12560624049f5e",
+    "url": "/projeto_01/static/js/main.d3822327.chunk.js"
+  },
+  {
+    "revision": "2c6fcdf7c57f7c3ed838",
+    "url": "/projeto_01/static/js/runtime-main.5520e554.js"
+  }
+]);
